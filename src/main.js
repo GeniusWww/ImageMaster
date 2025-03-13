8928edd86d05a3d2cba5900f15e6ded3a5b69399
yasong <1175261377@qq.com>
2025-03-13 21:24:42 +0800

    update watermark3

--- src/main.js
+++ src/main.js
@@ -4,7 +4,6 @@ import router from './router'
 
 // 导入Element Plus
 import ElementPlus from 'element-plus'
-import 'element-plus/dist/index.css'
 // 导入Element Plus图标
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 
@@ -21,3 +20,6 @@ for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
 }
 
 app.mount('#app')
+
+// 确保引入了 Element Plus 的样式
+import 'element-plus/dist/index.css'
